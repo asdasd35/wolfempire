9210c5f383c8b3c051c0a2d5516ca08504b021fe
asdasd35 <88936560+asdasd35@users.noreply.github.com>
2022-06-03 17:59:10 +0200

    asd

---
+++ afk.js
@@ -0,0 +1,44 @@
+const mineflayer = require('mineflayer');
+
+const botArgs = {
+    host: 'jewelfish.aternos.host',
+    port: '12460',
+    username: "afkbot"
+};
+
+const initBot = () => {
+
+    // Setup bot connection
+    let bot = mineflayer.createBot(botArgs);
+
+    bot.on('login', () => {
+        let botSocket = bot._client.socket;
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+    });
+
+    bot.on('message', message => {
+        console.log(`${message}`)
+    });
+
+    bot.on('login', async () => {
+        bot.chat("/login 123")
+    });
+
+    bot.on('end', () => {
+        console.log(`Disconnected`);
+
+        // Attempt to reconnect
+        setTimeout(initBot, 5000);
+    });
+
+    bot.on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+            console.log(`Failed to connect to ${err.address}:${err.port}`)
+        }
+        else {
+            console.log(`Unhandled error: ${err}`);
+        }
+    });
+};
+
+initBot();
